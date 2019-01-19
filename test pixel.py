9f2635278d8ba586a0d652f16b4a8f79a99d06e5
JessinRa <jessin.bdg@gmail.com>
2019-01-19 22:22:45 +0700

    Update pattern creator

--- test pixel.py
+++ test pixel.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-import string
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -7,7 +6,7 @@ import numpy as np
 import datetime
 
 
-def char_to_pixels(text, font_path, font_size):
+def char_to_pixels(text, font_path, font_size, text_char="1", blank_char="0"):
     font = ImageFont.truetype(font_path, font_size)
     width, height = font.getsize(text)
     height *= 2
@@ -16,9 +15,8 @@ def char_to_pixels(text, font_path, font_size):
     draw = ImageDraw.Draw(image)
     draw.text((0, 0), text, font=font)
 
-    matrix = np.asarray(image)
-    matrix = np.where(matrix, 0, 1)
-    matrix = matrix[(matrix != 0).any(axis=1)]
+    pixels = np.array(image, dtype=np.uint8)
+    matrix = np.array([text_char, blank_char], dtype="U1")[pixels]
     return matrix
 
 
@@ -41,7 +39,14 @@ def write_date(filename="result.txt"):
 def write_dimension(matrix, filename="result.txt"):
     height, width = matrix.shape
     with open(filename, 'a', encoding="utf-8") as file:
-        file.write("\n\n\n\n\nheight:{} width:{}".format(height, width))
+        file.write("{}\n{}\n".format(height, width))
+
+
+def write_content_only(matrix, filename="result.txt"):
+    with open(filename, 'a', encoding="utf-8") as file:
+        for row in matrix:
+            temp_row = "{}\n".format("".join([str(x) for x in row]))
+            file.write(temp_row)
 
 
 def write_matrix(matrix, filename="result.txt"):
@@ -60,22 +65,56 @@ def write_non_zero(matrix, filename="result.txt"):
         file.write("\n\n")
 
 
+def write_to_c_file(matrix, char, filename="result.h"):
+    height, width = matrix.shape
+
+    with open(filename, 'a', encoding="utf-8") as file:
+        file.write("#ifndef CONST_{}\n".format(char))
+        file.write("#define CONST_{}\n\n".format(char))
+
+        file.write("const int pattern_{}[{}][{}]=\n".format(char, height, width))
+        file.write("{\n")
+        for row in matrix:
+            file.write("\t{")
+            file.write("{}".format(", ".join([str(x) for x in row])))
+            file.write("},\n")
+        file.write("};\n")
+        file.write("#endif")
+
+
 if __name__ == "__main__":
 
-    write_date()
+    # write_date()
 
-    string_to_write = "Devi Habibi Harry Jessin Muharyman Nella Tony"
+    """
+    Create char by char pattern
+    """
+    # string_to_write = "Devi Habibi Harry Jessin Muharyman Nella Tony"
 
-    for char in string_to_write:
-        pixel_matrix = char_to_pixels(char, font_path='calibri/Calibri.ttf', font_size=400)
+    # for char in string_to_write:
+    #     pixel_matrix = char_to_pixels(char, font_path='calibri/Calibri.ttf', font_size=400)
+    #
+    #     # Std output
+    #     display_dimension(pixel_matrix)
+    #     # display_non_zero(pixel_matrix)
+    #     # print(pixel_matrix)
+    #     # print()
+    #
+    #     # File output
+    #     write_dimension(pixel_matrix, filename="pattern/pattern_{}.txt".format(char))
+    #     # write_non_zero(pixel_matrix, filename="pattern_{}.txt".format(char))
+    #     write_content_only(pixel_matrix, filename="pattern/pattern_{}.txt".format(char))
 
-        # Std output
+    """
+    Create merged name pattern
+    """
+
+    text = ["Devi", "Habibi", "Harry", "Jessin", "Muharyman", "Nella", "Tony"]
+    for name in text:
+        pixel_matrix = char_to_pixels(name, font_path='calibri/Calibri.ttf', font_size=400)
         display_dimension(pixel_matrix)
-        display_non_zero(pixel_matrix)
-        print(pixel_matrix)
-        print()
+        write_to_c_file(pixel_matrix, char=name, filename="pattern/const_pattern_{}.h".format(name))
 
-        # File output
-        write_dimension(pixel_matrix)
-        write_non_zero(pixel_matrix)
-        write_matrix(pixel_matrix)
+        # write_dimension(pixel_matrix, filename="pattern/name_pattern_{}.txt".format(name))
+        # # write_non_zero(pixel_matrix, filename="pattern_{}.txt".format(name))
+        # write_content_only(pixel_matrix, filename="pattern/name_pattern_{}.txt".format(name))
