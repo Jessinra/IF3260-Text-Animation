625e40b5cfb62ae9a617bdab52c2bfc245f8a0cf
JessinRa <jessin.bdg@gmail.com>
2019-01-19 09:31:32 +0700

    Write result to file

--- test pixel.py
+++ test pixel.py
@@ -4,6 +4,7 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 import numpy as np
+import datetime
 
 
 def char_to_pixels(text, font_path, font_size):
@@ -21,26 +22,60 @@ def char_to_pixels(text, font_path, font_size):
     return matrix
 
 
+def display_dimension(matrix):
+    height, width = matrix.shape
+    print("height:{} width:{}\n".format(height, width))
+
+
 def display_non_zero(matrix):
     result = np.where(matrix, '#', ' ')
     print('\n'.join([''.join(row) for row in result]))
 
 
-def display_dimension(matrix):
+def write_date(filename="result.txt"):
+    current_time = datetime.datetime.now().strftime("%d %m %y - %H:%M")
+    with open(filename, 'a', encoding="utf-8") as file:
+        file.write("======================= {} =======================".format(current_time))
+
+
+def write_dimension(matrix, filename="result.txt"):
     height, width = matrix.shape
-    print("height:{} width:{}".format(height, width))
+    with open(filename, 'a', encoding="utf-8") as file:
+        file.write("\n\n\n\n\nheight:{} width:{}".format(height, width))
+
+
+def write_matrix(matrix, filename="result.txt"):
+    with open(filename, 'a', encoding="utf-8") as file:
+        for row in matrix:
+            temp_row = "[{}]\n".format(", ".join([str(x) for x in row]))
+            file.write(temp_row)
+
+
+def write_non_zero(matrix, filename="result.txt"):
+    result = np.where(matrix, '#', ' ')
+
+    with open(filename, 'a', encoding="utf-8") as file:
+        file.write("\n\n")
+        file.write('\n'.join([''.join(row) for row in result]))
+        file.write("\n\n")
 
 
 if __name__ == "__main__":
 
-    for char in string.ascii_letters[:3]:
-        pixel_matrix = char_to_pixels(char, font_path='calibri/Calibri.ttf', font_size=32)
+    write_date()
 
+    string_to_write = "Devi Habibi Harry Jessin Muharyman Nella Tony"
+
+    for char in string_to_write:
+        pixel_matrix = char_to_pixels(char, font_path='calibri/Calibri.ttf', font_size=400)
+
+        # Std output
         display_dimension(pixel_matrix)
-
-        # Display in matrix version
-        print(pixel_matrix)
-
-        # Display with clear background
         display_non_zero(pixel_matrix)
+        print(pixel_matrix)
         print()
+
+        # File output
+        write_dimension(pixel_matrix)
+        write_non_zero(pixel_matrix)
+        write_matrix(pixel_matrix)
